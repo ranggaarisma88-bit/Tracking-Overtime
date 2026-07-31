d55716d5a88c66da0ac62d6a8cc31cb1ffe39773
ranggaarisma88-bit <ranggaarisma88@gmail.com>
2026-08-01 06:59:39 +0700

    Over Time Work

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-const CACHE = 'lembur-v1';
+const CACHE = 'lembur-v2';
+
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['.','index.html'])));
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['.', 'index.html'])));
     self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+        ))
+    );
     clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
 });
